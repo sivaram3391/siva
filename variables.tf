1891fdbd02675897610f1c0a2b97eb48bd298860
sivaram3391 <sivaraman3391@gmail.com>
2023-12-06 21:16:54 +0530

    iniiiii

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "number_of_subnets" {
   type=number
   description="This defines the number of subnets"
-  default =2
+  default =1
   validation {
     condition = var.number_of_subnets < 5
     error_message = "The number of subnets must be less than 5."
@@ -11,5 +11,5 @@ variable "number_of_subnets" {
 variable "number_of_machines" {
   type=number
   description="This defines the number of virtual machines"
-  default=2
+  default=1
 }
