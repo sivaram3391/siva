458e7ab5ca38a83d7c42d1f7f2e88e54ca3bdd5d
sivaram3391 <sivaraman3391@gmail.com>
2023-12-07 15:19:18 +0530

    client

--- provider.tf
+++ provider.tf
@@ -11,6 +11,6 @@ provider "azurerm" {
   subscription_id = "c1f5d20d-123c-4899-8141-128dd3704700"
   tenant_id = "8eed517b-da64-44a1-8624-83636749294a"
   client_id = "95ba5ee7-a6e7-4b76-b906-009d5fec44da"
-  client_secret = "38o8Q~MsAPo59FK.Zsq0eQImB-vW2wrJznAUqc9V"
+  client_secret = "hXp8Q~QtFHvM71JHzv~Mq4GaN7acr9pUOld9ydgW"
   features {}  
 }
