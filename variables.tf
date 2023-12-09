04c3231b104be83ddc70804604f4625bd05cb7f1
sivaram3391 <sivaraman3391@gmail.com>
2023-12-09 10:52:39 +0530

    2changes

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "number_of_subnets" {
   type=number
   description="This defines the number of subnets"
-  default =1
+  default =2
 }
 
 variable "number_of_machines" {
   type=number
   description="This defines the number of virtual machines"
-  default=1
+  default=2
 }
